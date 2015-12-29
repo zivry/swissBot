d6320bfbb2143f45d7cd3710fa5095ce4f4afcb3
nbdist <nbdist@itstl1061.iil.intel.com>
2015-12-29 14:48:00 +0200

    fix #8 will alert on wrong room

--- plugins/room.js
+++ plugins/room.js
@@ -1,6 +1,5 @@
 var Q = require('q');
 var _ = require('underscore');
-var url = require('url');
 var spawn = require('child_process').execFileSync;
 var moment = require('moment');
 
@@ -19,13 +18,14 @@ function exec(errorCodes,message, log, postMessage) {
 		{
 		 return errorCodes.reject_notHandling;
 		}
-		var building = message.length >  1 ? message[1] : 'IDC9'
-
+		var building = parseBuilding(message[1]);
+		if(building === undefined)
+			return errorCodes.reject_parsing;
+		//var building = message.length >  1 ? message[1] : 'IDC9'
 		log("will find a room in building:" + building);
 		var now = (new Date().getTime())+65*60*1000*2;
 		var starttime = moment().add(-10,'hours').format().split("+")[0];
 		var endtime = moment().add(10,'hours').format().split("+")[0];
-
 		var result	= JSON.parse(spawn("/usr/intel/bin/curl",["-s","--ntlm","-u",username+":"+password,'letsmeet.intel.com:8055/REST/Availability/GetRoomAvailabilityByBuildingLocal?organizerMailbox=zamir.ivry@intel.com&buildingName='+ building+ '&startTime=' + starttime + '&endTime=' + endtime + '&timeZone=Israel%20Standard%20Time&equipment=&minCapacity=0&apiKey=24D661C7-0605-4462-8A25-29B2C34653B9&requestor=undefined&format=JSON&logLevel=0']));
 		postMessage("found the following rooms:" + _.chain(result).filter(filter).pluck("ResourceName").value());	
 		return Q.when();
@@ -43,4 +43,16 @@ function exec(errorCodes,message, log, postMessage) {
 				}
 
 		}
+function parseBuilding(building)
+{
+	if(building === undefined) 
+		return "IDC9";
+	building = 	building.toUpperCase();
+	var buildings = JSON.parse(spawn("/usr/intel/bin/curl",["-s","--ntlm","-u",username+":"+password,'letsmeet.intel.com:8055/REST/Location/GetLocationsMetaData?apiKey=24D661C7-0605-4462-8A25-29B2C34653B9&requestor=adahan2&format=json&logLevel=4']));
+	var a = _.chain(buildings).map(function(item){return item.Sites}).flatten().pluck("Buildings").flatten().pluck("BuildingName").contains(building).value();
+	if(a)	
+		return building;	
+	postMessage("There is no such building: *" + building );	
+	return undefined;
+}
 };
