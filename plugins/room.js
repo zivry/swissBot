1b2c00ee78d211911d64d091c91fb4153b1ca4a6
Adi Dahan <adi.dahan@intel.com>
2015-12-30 17:41:10 +0200

    added bookRoom

--- plugins/room.js
+++ plugins/room.js
@@ -30,11 +30,13 @@ function exec(errorCodes,message, log, postMessage, user) {
 
 	//var building = message.length >  1 ? message[1] : 'IDC9'
 	var rooms = getRooms(building);
-	var	emptyRooms = _.chain(rooms).filter(filter).pluck("ResourceName").value();
-	if(emptyRooms.length === 0) {
+    var matchingRoom = _.chain(rooms).filter(filter);
+    var	emptyRoomName = matchingRoom.pluck("ResourceName").value();
+	if(emptyRoomName.length === 0) {
 		postMessage("could not find empty rooms in:" + building);
 	} else {
-		postMessage("found the following rooms:" + emptyRooms);
+		postMessage("found the following rooms:" + emptyRoomName);
+        bookRoom(matchingRoom);
 	}
 
 	return Q.when();
@@ -92,4 +94,33 @@ function exec(errorCodes,message, log, postMessage, user) {
 
 		return JSON.parse(spawn("/usr/intel/bin/curl",["-s", "--ntlm", "-u", username + ":" + password, url]));
 	}
+
+    function postData(url, options) {
+		var n = usersManager.getNegotiator();
+		var username = n.username;
+		var password = n.password;
+
+		return JSON.parse(spawn("/usr/intel/bin/curl",["-s", "--ntlm", "-u", username + ":" + password, "--data", '"' + options + '"', url]));
+	}
+
+    function bookRoom(room) {
+        var organizer = 'adi.dahan@intel.com';
+        var room_mail = room.ResourceEmail;//'IDC99108@intel.com';
+        var room_name = room.ResourceName;//'CR-IDC9-9108-12';
+        var start_time = '2015-12-30T17:30:00';
+        var end_time = '2015-12-30T18:00:00';
+        var booked_by = 'Booked%20by%20Dahan,%20Adi';
+        var time_zone = 'Israel%20Standard%20Time';
+        var api_key = '24D661C7-0605-4462-8A25-29B2C34653B9';
+        var requestor = 'adahan2';
+
+        var body = '%3Cdiv%3E---%20--%20--%20--%20--%20--%20--%20---%20Don%27t%20Edit%20or%20Remove%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%3C/div%3E%3Cdiv%3ERooms:CR-IDC9-9107-12%3C/div%3E%3Cdiv%3E--%20--%20--%20--%20--%20--%20--%20----%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20---%3C/div%3E';
+        var options = 'organizerMailbox=' + organizer + '&resourceEmail=' + room_mail + '&resourceName=' + room_name + '&startTime=' + start_time +
+                '&endTime=' + end_time + '&subject=' + booked_by + '&body=' + body + '&timeZone=' + time_zone + '&apiKey=' + api_key +
+                '&requestor=' + requestor + '&format=JSON&logLevel=0';
+
+        var url = 'http://letsmeet.intel.com:8055/REST/Reservation/CreateSingleReservationLocal';
+
+        postData(url, options);
+    }
 };
