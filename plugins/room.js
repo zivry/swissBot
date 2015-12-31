1c55b84f62d94d6e6a2951524d8b581439e8d99d
nbdist <nbdist@itstl1061.iil.intel.com>
2015-12-31 09:12:47 +0200

    #15 draft not working in curl

--- plugins/room.js
+++ plugins/room.js
@@ -4,6 +4,8 @@ var spawn = require('child_process').execFileSync;
 var moment = require('moment');
 var usersManager = require('../lib/usersManager');
 
+var request = require('sync-request');
+
 var DEFAULT_BUILDING = 'IDC9';
 
 module.exports = {
@@ -36,7 +38,7 @@ function exec(errorCodes,message, log, postMessage, user) {
 		postMessage("could not find empty rooms in:" + building);
 	} else {
 		postMessage("found the following rooms:" + emptyRoomName);
-        bookRoom(matchingRoom);
+        bookRoom(matchingRoom.value()[0]);
 	}
 
 	return Q.when();
@@ -94,33 +96,53 @@ function exec(errorCodes,message, log, postMessage, user) {
 
 		return JSON.parse(spawn("/usr/intel/bin/curl",["-s", "--ntlm", "-u", username + ":" + password, url]));
 	}
+ 
+   function bookRoom(room) {
 
-    function postData(url, options) {
-		var n = usersManager.getNegotiator();
-		var username = n.username;
-		var password = n.password;
+		var startDate = new Date();
+		var endDate = new Date();
+		//manual fix for GMT
+		startDate.setHours(startDate.getHours() + 2);
+		endDate.setHours(endDate.getHours() + 2);
 
-		return JSON.parse(spawn("/usr/intel/bin/curl",["-s", "--ntlm", "-u", username + ":" + password, "--data", '"' + options + '"', url]));
-	}
-
-    function bookRoom(room) {
-        var organizer = 'adi.dahan@intel.com';
+		if(startDate.getMinutes() < 30) {
+		    startDate.setMinutes(0);
+			endDate.setMinutes(30);
+		} else {
+		    startDate.setMinutes(30);
+			endDate.setMinutes(0);
+			endDate.setHours(endDate.getHours() + 1);
+		}
+		
+		var organizer = user.mail_address;//'adi.dahan@intel.com';
         var room_mail = room.ResourceEmail;//'IDC99108@intel.com';
         var room_name = room.ResourceName;//'CR-IDC9-9108-12';
-        var start_time = '2015-12-30T17:30:00';
-        var end_time = '2015-12-30T18:00:00';
-        var booked_by = 'Booked%20by%20Dahan,%20Adi';
-        var time_zone = 'Israel%20Standard%20Time';
+        var start_time = parseDateString(startDate);//'2015-12-30T17:30:00';
+        var end_time = parseDateString(endDate);//'2015-12-30T18:00:00';
+        var booked_by = 'Booked by ' + user.mail_name;//Dahan, Adi';
+        var time_zone = 'Israel Standard Time';
         var api_key = '24D661C7-0605-4462-8A25-29B2C34653B9';
-        var requestor = 'adahan2';
+        var requestor = user.id;//'adahan2';
 
         var body = '%3Cdiv%3E---%20--%20--%20--%20--%20--%20--%20---%20Don%27t%20Edit%20or%20Remove%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%3C/div%3E%3Cdiv%3ERooms:CR-IDC9-9107-12%3C/div%3E%3Cdiv%3E--%20--%20--%20--%20--%20--%20--%20----%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20--%20---%3C/div%3E';
         var options = 'organizerMailbox=' + organizer + '&resourceEmail=' + room_mail + '&resourceName=' + room_name + '&startTime=' + start_time +
                 '&endTime=' + end_time + '&subject=' + booked_by + '&body=' + body + '&timeZone=' + time_zone + '&apiKey=' + api_key +
                 '&requestor=' + requestor + '&format=JSON&logLevel=0';
 
-        var url = 'http://letsmeet.intel.com:8055/REST/Reservation/CreateSingleReservationLocal';
-
-        postData(url, options);
+        var url = 'http://letsmeet.intel.com:8055/REST/Reservation/CreateSingleReservationLocal?' + options;
+		post(url);
     }
+
+	function parseDateString(date) {
+		var dateStr = date.toISOString();
+		return dateStr.substring(0, dateStr.indexOf('.'));
+	}
+
+	function post(url) {
+	    var response = request('POST', url);
+	    if (response.statusCode > 299) {
+	        console.log('HTTP ERROR ' + response.statusCode + ': %j', response.body)
+	    }
+	    return response.getBody('utf8');
+	}
 };
